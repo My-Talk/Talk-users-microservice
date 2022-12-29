64ec41050d40a703cd4c2b206d6b49fc30bce018
ngimdock <ngimdockzemfack@gmail.com>
2022-12-29 10:44:47 +0100

    Fixing id to _id

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -4,7 +4,7 @@ import { User } from 'src/mongodb/schemas';
 
 export class UserEntity {
   @ApiProperty()
-  private id: string;
+  private _id: string;
 
   @ApiProperty()
   private username: string;
@@ -38,7 +38,7 @@ export class UserEntity {
   }
 
   init(userData: User) {
-    this.id = userData._id;
+    this._id = userData._id;
     this.username = userData.username;
     this.name = userData.name;
     this.phone = userData.phone;
@@ -51,20 +51,22 @@ export class UserEntity {
     this.hashRt = userData.hashRt;
   }
 
-  get getDatas() {
+  get getDatas(): User {
     return {
-      id: this.id,
+      _id: this._id,
       username: this.username,
       name: this.name,
       phone: this.phone,
       avatar: this.avatar,
       createdAt: this.createdAt,
       updatedAt: this.updatedAt,
+      hash: this.hash,
+      hashRt: this.hashRt,
     };
   }
 
   get getId() {
-    return this.id;
+    return this._id;
   }
 
   get getUsername() {
